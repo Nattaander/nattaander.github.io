ab594c5459577da436738e8ae7773117cbb53a1c
Nattaander <bolgeraaron@gmail.com>
2015-04-21 21:12:15 +0100

    gahh

--- infoTestSite/responder.php
+++ infoTestSite/responder.php
@@ -1,12 +1,15 @@
-<html>
-<body>
-<?php
-
-function get_client_ip() {
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <?php
+  function get_client_ip() {
     $ipaddress = $_SERVER['REMOTE_ADDR'];
     return $ipaddress;
 }
-
+  ?>
+  </head>
+<body>
+<?php
 echo get_client_ip();
 echo "echos";
 ?>
